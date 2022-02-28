4255f0da22200f1f3972fa0e00b680cebdfb43ec
Guilherme <guilherme.salviano@hotmail.com>
2022-02-28 10:29:40 -0300

    test 1

--- tests/unit/components/entryRecord/LicensePlate.spec.js
+++ tests/unit/components/entryRecord/LicensePlate.spec.js
@@ -4,6 +4,6 @@ import { mount } from "@vue/test-utils"
 test('It tests whether the emit is working', ()=>{
     const wrapper = mount(LisencePlate)
     wrapper.setData({licensePlate: '12345'})
-    wrapper.vm.$emit('emitLicensePlateNumber', wrapper.vm.licensePlate, 'licensePlate')
+    wrapper.vm.$emit('emitLicensePlateNumber')
     console.log(wrapper.emitted('emitLicensePlateNumber'))
 })
