0e9243cae332158ac3a2fa61fe3fc351f6b866cd
Guilherme <guilherme.salviano@hotmail.com>
2022-03-16 20:36:05 -0300

    dailyRateCost component created

---
+++ src/components/priceRecord/DailyRateCost.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="drcost">
+        <h2>Preço da diária</h2>
+        <label for="cost">Preço:</label>
+        <input type="number" id="cost" v-model="dailyRateCost">
+        <button>Definir</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            dailyRateCost: ''
+        }
+    },
+    methods:{
+        setDailyRateCost(){
+            this.$emit('emitDailyRateCost', this.dailyRateCost)
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/priceRecord/PriceRecord.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="price">
+        <my-header></my-header>
+        <h1>Tabela de preços</h1>
+        <daily-rate-cost :emitDailyRateCost='defineDailyRateCost'></daily-rate-cost>
+    </div>
+</template>
+<script>
+import MyHeader from '../shared/MyHeader.vue'
+import DailyRateCost from '../priceRecord/DailyRateCost.vue'
+export default {
+    components:{
+        MyHeader,
+        DailyRateCost
+    },
+    data(){
+        return{
+            dailyRateCost: ''
+        }
+    },
+    methods:{
+        defineDailyRateCost(price){
+            this.dailyRateCost = price
+        }
+    }
+}
+</script>
+<style scoped>
+    .price{
+        width: 100%;
+    }
+</style>
