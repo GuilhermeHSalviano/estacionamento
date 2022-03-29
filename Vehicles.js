f60c84d1ef929e6df0e0d756b385510fa16d3f4b
Guilherme <guilherme.salviano@hotmail.com>
2022-03-29 20:08:06 -0300

    creating prices for daily rate and motorcycles

--- Vehicles.js
+++ Vehicles.js
@@ -1,11 +1,11 @@
-import { getDate } from "./functions"
-
 export class Vehicle{
     constructor(car = '', licensePlate = '', entryDate = '', entryHour = '', dailyRate = ''){
+        /*this.car define whether the vehicle is a car or a motorcycle.*/
         this.car = car 
         this.licensePlate = licensePlate
         this.entryDate = entryDate
         this.entryHour = entryHour
+        /*this.dailyRate defines whether the stay is daily rate or not.*/
         this.dailyRate = dailyRate
     
     }
